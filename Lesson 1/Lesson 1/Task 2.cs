da6aae3b5bc6911cc5a9ae2259e4e629c7e543c4
alexgrist14 <alexgrist133@gmail.com>
2020-02-04 14:41:16 +0300

    refactor: change project structure

---
+++ Lesson 1/Lesson 1/Task 2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    public class Task2
+    {
+        public static int BinaryGCD(int u, int v)
+        {
+            int shift = 0;
+            if (u == 0) return v;
+            if (v == 0) return u;
+
+            while (((u | v) & 1) == 0)
+            {
+                shift++;
+                u >>= 1;
+                v >>= 1;
+            }
+
+            while ((u & 1) == 0)
+            {
+                u >>= 1;
+            }
+
+            do
+            {
+                while ((v & 1) == 0)
+                {
+                    v >>= 1;
+                }
+
+                if (u > v)
+                {
+                    int t = v; v = u; u = t;
+                }
+
+                v -= u;
+            } while (v != 0);
+
+            return u << shift;
+        }//task2
+    }
+}
---
+++ Lesson 1/Lesson 1/Task1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    public class Task1
+    {
+
+        public static void euclideanAlgorithm(int a, int b)
+        {
+            int NOD;
+            int temp = a;
+            int q = 0;
+            while (temp != 0)
+            {
+
+                if (temp >= b)
+                {
+                    temp = temp - b;
+                    q++;
+                }
+
+                else
+                {
+                    NOD = q * b + temp;
+                    b = temp;
+                    temp = NOD;
+                    q = 0;
+                }
+            }
+
+            Console.WriteLine("Наибольший общий делитель: " + b);
+
+        } // task 1
+
+    }
+}
