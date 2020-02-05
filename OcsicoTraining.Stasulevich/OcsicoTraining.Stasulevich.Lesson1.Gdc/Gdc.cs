cbe70f79786d6f533e5722a205ca653c04a37c67
alexgrist14 <43311575+alexgrist14@users.noreply.github.com>
2020-02-05 18:46:40 +0300

    feature: rebuild project (#3)

---
+++ OcsicoTraining.Stasulevich/OcsicoTraining.Stasulevich.Lesson1.Gdc/Gdc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OcsicoTraining.Stasulevich.Lesson1.Gdc
+{
+    public static class Gdc
+    {
+        public static int Calculate(int a, int b)
+        {
+            int nod;
+            int temp = a;
+            int currentStep = 0;
+
+            while (temp != 0)
+            {
+                if (temp >= b)
+                {
+                    temp = temp - b;
+                    currentStep++;
+                }
+                else
+                {
+                    nod = currentStep * b + temp;
+                    b = temp;
+                    temp = nod;
+                    currentStep = 0;
+                }
+            }
+
+            return b;
+        }
+    }
+}
