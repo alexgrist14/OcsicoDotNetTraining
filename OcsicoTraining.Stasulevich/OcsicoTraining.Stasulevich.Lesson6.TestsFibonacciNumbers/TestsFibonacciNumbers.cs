2584b6a5a1d5e42c3fb3f6ea1537e7794292bb75
alexgrist14 <43311575+alexgrist14@users.noreply.github.com>
2020-02-19 11:42:09 +0300

    Lesson 6 task 4 (#11)

---
+++ OcsicoTraining.Stasulevich/OcsicoTraining.Stasulevich.Lesson6.TestsFibonacciNumbers/TestsFibonacciNumbers.cs
@@ -0,0 +1,50 @@
+using System.Linq;
+using NUnit.Framework;
+
+namespace OcsicoTraining.Stasulevich.Lesson6.TestsFibonacciNumbers
+{
+    [TestFixture]
+    public class TestsFibonacciNumbers
+    {
+        [Test]
+        public void FibonacciSequence_InputCountOfNumbers7_ReturnCollectionWithSevenNumbers()
+        {
+            //Arrange
+            var number = 7;
+            var expected = 7;
+
+            //Act
+            var actual = FibonacciNumbers.FibonacciNumbers.FibonacciSequence(number).Count();
+
+            //Assert
+            Assert.AreEqual(expected, actual, "count of number should be seven");
+        }
+
+        [Test]
+        public void FibonacciSequence_InputCountOfNumbers10_ReturnTenthNumberIs34()
+        {
+            //Arrange
+            var number = 10;
+            var expected = 34;
+
+            //Act
+            var actual = FibonacciNumbers.FibonacciNumbers.FibonacciSequence(number).Last();
+
+            //Assert
+            Assert.AreEqual(expected, actual, double.Epsilon, "tenth number should be 34");
+        }
+
+        [Test]
+        public void FibonacciSequence_InputNegativeNumber1_ShouldThrowException()
+        {
+            //Arrange
+            var number = -1;
+
+            //Act
+            void GetException() => FibonacciNumbers.FibonacciNumbers.FibonacciSequence(number).ToList();
+
+            //Assert
+            Assert.Throws<System.ArgumentException>(GetException);
+        }
+    }
+}
