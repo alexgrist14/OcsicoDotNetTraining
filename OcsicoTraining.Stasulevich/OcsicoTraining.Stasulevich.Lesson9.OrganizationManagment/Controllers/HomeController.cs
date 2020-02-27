dd602a8687645f56cf094156f0f93a8bf73097f8
alexgrist14 <agurec.fasasf@gmail.com>
2020-02-27 18:00:11 +0300

    initial commit

---
+++ OcsicoTraining.Stasulevich/OcsicoTraining.Stasulevich.Lesson9.OrganizationManagment/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OcsicoTraining.Stasulevich.Lesson9.OrganizationManagment.Models;
+
+namespace OcsicoTraining.Stasulevich.Lesson9.OrganizationManagment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
